a579e573ca861294917ccb753e16989cb2b35a5f
unknown <premdeshmane01@gmail.com>
2025-12-21 02:25:01 +0530

    Add robots.txt for SEO

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://portfolio-prem-4m65.vercel.app//sitemap.xml",
+  };
+}
